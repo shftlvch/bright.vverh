99636c764cfc25319a206c5a31a1a648233dd617
Daniil Sheftelevich <shftlvch@gmail.com>
2017-12-04 18:43:37 +0300

    go go power rangers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
 
 import "./App.css";
+import logo from './assets/logo.png';
 import "./assets/font-awesome/css/font-awesome.css";
 import $ from "jquery";
 import Typed from "typed.js";
+import request from "superagent";
+import Dropzone from 'react-dropzone'
+
+const CLOUDINARY_UPLOAD_PRESET = 'b9txgygb';
+const CLOUDINARY_UPLOAD_URL = 'https://api.cloudinary.com/v1_1/vverh/upload';
 
 class App extends Component {
     constructor(props) {
@@ -16,12 +22,14 @@ class App extends Component {
                 'я пою',
                 'винишко и друзья',
                 'прихожу в Вверх',
-            ]
+            ],
+            fileUrl: '',
         };
 
 
         this.changeIcon = this.changeIcon.bind(this);
         this.changePlaceholder = this.changePlaceholder.bind(this);
+        this.onDrop = this.onDrop.bind(this);
     }
 
     componentDidMount() {
@@ -52,8 +60,32 @@ class App extends Component {
             placeholder = placeholders.length - 1 > this.state.placeholder ? (this.state.placeholder + 1) : 0;
 
 
-
         this.setState({placeholder});
+    }
+
+    onDrop(files) {
+        this.setState({
+            uploadedFile: files[0]
+        });
+        this.handleImageUpload(files[0]);
+    }
+
+    handleImageUpload(file) {
+        let upload = request.post(CLOUDINARY_UPLOAD_URL)
+            .field('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+            .field('file', file);
+
+        upload.end((err, response) => {
+            if (err) {
+                console.error(err);
+            }
+
+            if (response.body.secure_url !== '') {
+                this.setState({
+                    fileUrl: response.body.secure_url
+                });
+            }
+        });
     }
 
     render() {
@@ -62,15 +94,17 @@ class App extends Component {
             placeholder = state.placeholders[state.placeholder];
         return (
             <div>
-                <div className="form-wrapper">
-                    <form method="POST" encType="multipart/form-data" className="form">
-
+                <div className="image" style={{backgroundImage: 'url(' + this.state.fileUrl + ')'}}/>
+                {/*<div className="logo"><img src={logo}/></div>*/}
+                <div className="form-wrapper" style={{backgroundColor: this.state.fileUrl ? 'rgba(229, 106, 99, .8)' : 'rgba(229, 106, 99, 1)'}}>
+                    <form method="POST" className="form">
                         <header className="form__header">#мне_не_серо_когда</header>
                         <input className="form__input" placeholder={placeholder}/>
-                        <input className="form__file" type="file" name="img" accept="image/jpeg,image/png,image/gif"/>
-                        <label className="form__file-label" htmlFor="form__file"><i className={"fa " + icon}
-                                                                                    aria-hidden="true"></i>Сэлфи или
-                            картинка</label>
+                        <Dropzone className="form__file" onDrop={this.onDrop.bind(this)} accept="image/jpeg,image/png,image/gif">
+                            <i className={"fa " + icon} aria-hidden="true"/>
+                            Сэлфи или картинка
+                        </Dropzone>
+
                         {/*<input className="form__btn" type="submit" value="Отправить"/>*/}
                     </form>
                 </div>
